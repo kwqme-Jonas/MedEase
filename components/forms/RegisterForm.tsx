006ce942feb35cb37f16acd32e238e6d89f77867
kwqme-jonas <donkorjonas00@gmail.com>
2025-01-26 15:45:19 +0000

    updated register page

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -121,7 +121,27 @@ const RegisterForm = ({ user }: { user: User }) => {
                 />
             </div>
 
+            {/*<section className="space-y-6">
+                <div className="mb-9 space-y-1">
+                    <h2 className="sub-header">Medical Information</h2>
+                </div>
+            </section>*/}
+
             <div className="flex flex-col gap-6 xl:flex-row">
+                <CustomFormField 
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="address"
+                    label="Address"
+                    placeholder="Ashaiman, Middle-East"
+                />
+                 <CustomFormField 
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="occupation"
+                    label="Occupation"
+                    placeholder="Software Engineer"
+                />
 
             </div>
         
