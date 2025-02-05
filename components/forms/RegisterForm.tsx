c9e50fee16f3317a3b38e109b1bb216231ac64e2
kwqme-jonas <donkorjonas00@gmail.com>
2025-02-05 22:09:14 +0000

    updated register form

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -274,9 +274,37 @@ const RegisterForm = ({ user }: { user: User }) => {
                     label="Scanned copy of identification"
                     renderSkeleton={(field) => (
                        <FormControl>
-                        <FileUploader/>
+                        <FileUploader
+                            files={field.value}
+                            onChange={field.onChange}
+                        />
                        </FormControl>
                     )}
+            />
+
+            <section className="space-y-6">
+                <div className="mb-9 space-y-1">
+                    <h2 className="sub-header">Consent and Privacy</h2>
+                </div>
+            </section>
+
+            <CustomFormField 
+                fieldType={FormFieldType.CHECKBOX}
+                control={form.control}
+                name="treatmentConsent"
+                label="I consent to treatment"
+            />
+            <CustomFormField 
+                fieldType={FormFieldType.CHECKBOX}
+                control={form.control}
+                name="disclosureConsent"
+                label="I consent to disclosure of information"
+            />
+            <CustomFormField 
+                fieldType={FormFieldType.CHECKBOX}
+                control={form.control}
+                name="privacyConsent"
+                label="I consent to privacy policy"
             />
 
 
