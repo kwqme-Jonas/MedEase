65dddc3d25aa00a23a0626759f73e7d4776a8e77
kwqme-Jonas <donkorjonas00@gmail.com>
2026-03-07 14:11:37 +0000

    update data storage

--- lib/actions/patients.actions.ts
+++ lib/actions/patients.actions.ts
@@ -1,7 +1,10 @@
+import { InputFile } from 'node';
 'use server'
-import { ID, Query } from "node-appwrite"
-import { users } from "../appwrite.config"
+import { Databases, ID, Query } from "node-appwrite"
+import { BUCKET_ID, DATABASE_ID, PATIENT_COLLECTION_ID, PROJECT_ID, storage, users } from "../appwrite.config"
 import { parseStringify } from "../utils";
+
+import { InputFile } from "node-appwrite/file";
 
 export const createUser = async (user : CreateUserParams) => {
     try {
@@ -32,6 +35,33 @@ export const getUser = async (userId: string) => {
         const user = await users.get(userId);
 
         return parseStringify(user);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const registerPatient = async ({ identificationDocument, ...patient }: RegisterUserParams) => {
+    try {
+        let file;
+
+        if(identificationDocument){
+            const inputFile = InputFile.fromBuffer(identificationDocument?.get('blobFile') as Blob,
+            identificationDocument?.get('fileName') as string,
+        )
+
+        file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile)
+        }
+
+        const patient = await Databases.createDocument(
+            DATABASE_ID!,
+            PATIENT_COLLECTION_ID!,
+            ID.unique(),
+            {
+                identificationDocument: file?.$id || null,
+                identificationDocumentUrl: `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}`,
+                ...patient
+            }
+        )
     } catch (error) {
         console.log(error);
     }
